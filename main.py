eeb61df98257acd5fb2bbe4917a6d3a5dfd104aa
Levi Crisostomo <137958145+Levi-Cris@users.noreply.github.com>
2023-06-29 18:23:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,42 +2,37 @@ import os
 import pandas as pd
 import plotly.express as px
 
+def separador_tabela():
+    print('')
+    print('-=' * 20)
+    print('')
+
 lista_arquivo = os.listdir("X:\Arquivos\Vendas")
 tabela_total = pd.DataFrame()
 
 for arquivo in lista_arquivo:
-    #print(arquivo) - mostra o nome de todos os arquivos no diretório
     if "Vendas" in arquivo:
-        #print(f"D:\Vendas\{arquivo}") - mostra apenas os arquivos de venda do diretório
         tabela = pd.read_csv(f"X:\Arquivos\Vendas\{arquivo}")
-        #print(arquivo)
-        #print(tabela)
         tabela_total = tabela_total._append(tabela)
-#print(tabela_total) - mostra uma única lista com as informações de vendas de todas as lojas
 
-#agrupar produtos de acordo com as vendas:
+#agrupar produtos de acordo com o número de vendas:
 tabela_produtos = tabela_total.groupby('Produto').sum()
 tabela_produtos = tabela_produtos[['Quantidade Vendida']].sort_values(by='Quantidade Vendida', ascending=False)
 print(tabela_produtos)
-print('')
-print('-=' * 20)
-print('')
+separador_tabela()
 
 #produto com maior faturamento:
 tabela_total['Faturamento'] = tabela_total['Quantidade Vendida'] * tabela_total['Preco Unitario']
 tabela_faturamento = tabela_total.groupby('Produto').sum()
 tabela_faturamento = tabela_faturamento[['Faturamento']].sort_values(by='Faturamento', ascending=False)
 print(tabela_faturamento)
-print('')
-print('-=' * 20)
-print('')
+separador_tabela()
 
 #loja que mais vendeu em faturamento:
 tabela_lojas = tabela_total.groupby('Loja').sum()
 tabela_lojas = tabela_lojas[['Faturamento']].sort_values(by='Faturamento', ascending=False)
 print(tabela_lojas)
-print('')
-print('-=' * 20)
+separador_tabela()
 
 #gerando gráficos através dos dados:
 grafico1 = px.bar(tabela_produtos, x=tabela_produtos.index, y='Quantidade Vendida')
